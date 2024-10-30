c226b1ccf810120bad4b06177569daa1a43902c1
bekir.cetin <bekir9090@gmail.com>
2024-10-30 16:20:44 +0300

    1. deneme

---
+++ src/GitHub/_01_GİT.java
@@ -0,0 +1,21 @@
+package GitHub;
+
+public class _01_GİT {
+    public static void main(String[] args) {
+
+        public class okul{
+
+            private int okulNo;
+            private String okulAd;
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
