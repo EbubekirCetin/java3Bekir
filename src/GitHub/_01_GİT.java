ebf0cad60e653c58db9bca9fc79aaa4eadf3bf57
bekir.cetin <bekir9090@gmail.com>
2024-10-31 09:36:57 +0300

    2. deneme

--- src/GitHub/_01_GİT.java
+++ src/GitHub/_01_GİT.java
@@ -3,10 +3,13 @@ package GitHub;
 public class _01_GİT {
     public static void main(String[] args) {
 
-        public class okul{
+        class okul{
 
             private int okulNo;
             private String okulAd;
+
+
+            private int sayi;
 
 
 
